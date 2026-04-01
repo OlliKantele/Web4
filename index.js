dddd690cec6f300c7742e1168bb593899c7e6ac2
OlliKantele <otkant@utu.fi>
2026-04-01 19:49:27 +0300

    1

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+const cors = require ("cors")
+app.use(express.json())
+app.use(morgan("tiny"))
+app.use(cors())
+app.use(express.static("dist"))
+
+let persons = [
+  { "id": "1", "name": "Arto Hellas", "number": "040-123456" },
+  { "id": "2", "name": "Ada Lovelace", "number": "39-44-5323523" },
+  { "id": "3", "name": "Dan Abramov", "number": "12-43-234345" },
+  { "id": "4", "name": "Mary Poppendieck", "number": "39-23-6423122" }
+]
+
+app.get('/api/persons', (req, res) => res.json(persons))
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find(p => p.id === req.params.id)
+  person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(p => p.id !== req.params.id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' })
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000000)),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log("server running on port ${PORT}"))
